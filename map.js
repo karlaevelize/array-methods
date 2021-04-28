9a4a2d01655e7167a31599c3f722c4d8556cbde5
karlaevelize <k.evelize@gmail.com>
2021-04-28 11:01:17 +0200

    map examples

--- map.js
+++ map.js
@@ -6,9 +6,12 @@ const data = require("./data")
 
 //array of first name
 const arrayFirstName = data.map(students => students.firstName)
-console.log(arrayFirstName)
+//console.log(arrayFirstName)
 
 //array of full names
-
+const arrayFullNames = data.map(students => `${students.firstName} ${students.lastName}`)
+//console.log(arrayFullNames)
 
 //another array of full names
+const anotherFullName = data.map(students => students.firstName + " " + students.lastName)
+//console.log(anotherFullName)
