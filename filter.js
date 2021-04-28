2c89917b754b92f49ffd0b51c129ac5d9db4dc2f
karlaevelize <k.evelize@gmail.com>
2021-04-28 10:52:17 +0200

    examples for filter

--- filter.js
+++ filter.js
@@ -1,9 +1,26 @@
 //What does filter do?
 
+//It returns everything that matches the condition
+//Returns an array
+
+const data = require("./data")
+
 //filter by house
+const filterByHouse = data.filter(students => students.house === "Ravenclaw")
+//console.log(filterByHouse)
 
 //filter by id
+const filterbyId = data.filter(student => student.id === 1)
+//console.log(filterbyId)
+
+//filter by id > 5
+const filterByIdMore = data.filter(students => students.id > 5)
+//console.log(filterByIdMore)
 
 //filter by plays Quidditch
+const filterPlaysQuidditch = data.filter(students => students.playsQuidditch)
+//console.log(filterPlaysQuidditch)
 
 //filter by plays Quidditch and gender === "m"
+const filterByPlaysQuidditchAndMale = data.filter(students => students.gender === "m" && students.playsQuidditch)
+//console.log(filterByPlaysQuidditchAndMale)
--- map.js
+++ map.js
@@ -1,7 +1,14 @@
 //What does map do?
+//It returns an array, the same size as the original one
+//New array, same size, new data
+
+const data = require("./data")
 
 //array of first name
+const arrayFirstName = data.map(students => students.firstName)
+console.log(arrayFirstName)
 
 //array of full names
+
 
 //another array of full names
